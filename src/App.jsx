0a4df95f4d0af9684b193a49522e97f04543c10b
Ahmad Zidan Ali <ahmadzidanali909@gmail.com>
2025-12-05 21:42:10 +0700

    Initialize course show page.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { AppLayout } from "./layouts/AppLayout";
 import { HomePage } from "./pages/HomePage";
 import { LoginPage } from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
+import CourseShowPage from "./pages/course/CourseShowPage";
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
             <Route index element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
+            <Route path="/courses" element={<CourseShowPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
