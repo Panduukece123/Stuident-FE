2c750054435ee00b97a41508e91656c9e40db627
Bima Adnandita <bima.adnandita@gmail.com>
2026-01-11 20:36:38 +0700

    fix: refactoring beberapa code di halaman  elearning

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,18 @@
+// File: src/hooks/useDebounce.js
+import { useState, useEffect } from "react"; // <--- JANGAN LUPA INI
+
+export default function useDebounce(value, delay) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
